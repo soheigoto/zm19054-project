22875934cdfd4c13941a9abad9c5fdd1d1f3339e
soheigoto <zm19054@josai.ac.jp>
2021-02-06 12:29:14 +0000

    2/6

--- ex06-1.js
+++ ex06-1.js
@@ -1,1 +1,3 @@
-function txtchange1(){document.getElementById('txt1').innerHTML='略称はアンデラ！死と幸運を否定した言葉は？';}
+function txtchange1() {
+  document.getElementById('txt1').innerHTML = '略称はアンデラ！死と幸運を否定した言葉は';
+}
